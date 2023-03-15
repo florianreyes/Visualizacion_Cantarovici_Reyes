6f4e76af5144a4c946e9c379f32051744e4175d4
nicocanta20 <nicocanta20@gmail.com>
2023-03-15 10:13:40 -0300

    add legend

--- vd_s1_tp2_Cantarovici_Reyes/dataviz_4/script.js
+++ vd_s1_tp2_Cantarovici_Reyes/dataviz_4/script.js
@@ -4,13 +4,17 @@ d3.csv("astronautas.csv", d3.autoType).then((data) => {
 
   let chart = Plot.plot({
     marks: [
-      Plot.barY(data, {
+      Plot.barY(data, 
+        {
         x: "nacionalidad",
         y: "mision_hs",
-        fill: "ocupacion",
-        sort: "ocupacion",
+        fill: "ocupacion",  
+        // sort: {x:"y",reverse:true},  
+        sort:"ocupacion",
         title: (d) => `${d.ocupacion}: ${d.mision_hs} hs`,
-      }),
+      }
+      
+      ),
     ],
     grid: true,
     line: true,
@@ -20,6 +24,10 @@ d3.csv("astronautas.csv", d3.autoType).then((data) => {
     height: 500,
     marginLeft: 100,
     marginBottom: 60,
+    color: {
+      legend: true,
+    },
+    
   });
   // Agregamos chart al div#chart de index.html
   d3.select("#chart").append(() => chart);
